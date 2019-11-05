875a89c6261e169c022c7e96935908c0930b933f
Soham <sohamk2297@gmail.com>
2019-11-05 20:57:04 +0530

    3rd commit

--- first.js
+++ first.js
@@ -1,7 +1,10 @@
 var http = require("http");
+var url = require('url');
 var serv = http.createServer(function (req,res) {
 res.writeHead(200,{'Content-Type':'text/html'});
 res.write("<html><body><h2>HELLO WORLD</h2></body></html>");
-res.end();
+var q = url.parse(req.url,true).query;
+var txt = q.year + " " + q.month;
+res.end(txt);
 });
 serv.listen(8081);
