75f512e87f4da92e72657e06da08e93ba243352d
Soham <sohamk2297@gmail.com>
2019-11-05 20:33:55 +0530

    first code commit

---
+++ first.js
@@ -0,0 +1,6 @@
+var http = require("http");
+var serv = http.createServer(function (req,res) {
+res.writeHead(200,{'Content-Type':'text/html'});
+res.write("<html><body><h2>HELLO WORLD</h2></body></html>")    
+});
+serv.listen(8081);
