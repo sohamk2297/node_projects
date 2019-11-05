5daf01c2e6cd3ced991e83b5fb85ee1a9efb7d7b
Soham <sohamk2297@gmail.com>
2019-11-05 21:14:57 +0530

    Added fs module

--- first.js
+++ first.js
@@ -1,10 +1,20 @@
 var http = require("http");
 var url = require('url');
-var serv = http.createServer(function (req,res) {
-res.writeHead(200,{'Content-Type':'text/html'});
-res.write("<html><body><h2>HELLO WORLD</h2></body></html>");
-var q = url.parse(req.url,true).query;
-var txt = q.year + " " + q.month;
-res.end(txt);
+var fs = require('fs');
+var serv = http.createServer(function (req, res) {
+    fs.readFile("index.html", function (err,data){
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        if(data)
+        {
+            var q = url.parse(req.url, true).query;
+            var txt = q.year + " " + q.month;
+            res.end(data);
+        }
+        else
+        {
+            res.end(err.name);
+        }
+    });
+
 });
 serv.listen(8081);
